7155b2ca86c6d4a19329581df1f7873285f56b26
Eugene9533 <79876217+Eugene9533@users.noreply.github.com>
2022-05-23 09:08:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MirapolisExtension
 // @namespace    http://tampermonkey.net/
-// @version      0.30
+// @version      0.31
 // @description  Adding new expanding functionality
 // @author       Eugene
 // @match        https://mv1.virtualroom.ru/*
